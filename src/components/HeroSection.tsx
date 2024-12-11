41c1c3b83cb1f9e8eec85b71921039f3328cb8e2
Chaman Chaudhary <chaudharychaman1506@gmail.com>
2024-12-11 21:56:43 +0530

    complete UI

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,21 +1,47 @@
 const HeroSection = () => {
   return (
-    <div className="h-screen w-full herosection-bg-image flex flex-col justify-center items-center space-y-20">
-      <div className="">
-        <h1 className="uppercase text-8xl font-bold herosection-transparent-heading relative z-10">
+    <div className="h-screen w-full herosection-bg-image flex flex-col justify-around items-center space-y-20">
+      <div className="slide-down w-full relative lg:-top-5 top-0">
+        <h1 className="uppercase lg:text-8xl md:text-6xl text-6xl overflow-hidden flex justify-center font-bold herosection-transparent-heading relative z-10">
           cybertruck
         </h1>
-        <div className="relative -top-14 z-20 flex flex-col justify-center items-center space-y-2">
-          <h2 className="uppercase text-7xl font-bold">cybertruck</h2>
-          <p className="w-1/2 text-center text-[8px]">
+        <div className="relative lg:-top-14 -top-5 z-20 flex flex-col justify-center items-center space-y-2">
+          <h2 className="uppercase lg:text-7xl md:text-6xl text-5xl font-bold">
+            cybertruck
+          </h2>
+          <p className="w-1/2 lg:w-2/5 text-center lg:text-[9px] text-[6px]">
             durable and rugged enough to go anywhere. tackle anything with
             electronically adaptive air suspension that offers 12" of travel and
             17" of clearance.
           </p>
         </div>
       </div>
-      <div>
-        <span className="uppercase font-semibold">Adventure</span>
+      <div className="w-4/5 flex justify-center md:justify-between items-start text-[7px] slide-up">
+        <div className="hidden lg:flex flex-col items-start justify-center w-60">
+          <span className="text-sm">ADVENTURE GOING</span>
+          <span>
+            Lorem ipsum dolor sit amet adipisicing Ut dolores iure? dolor Dicta,
+            autem!
+          </span>
+        </div>
+        <div className="flex items-start justify-center space-x-12 w-full">
+          <div className="flex flex-col items-center justify-center">
+            <span className="text-sm">11,000 LBS</span>
+            <span>TOWING CAPACITY</span>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <span className="text-sm">350 MILES</span>
+            <span>EST RANGE</span>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <span className="text-sm">2.0 SEC</span>
+            <span>0-60 MPH</span>
+          </div>
+        </div>
+        <div className="hidden lg:flex flex-col items-start justify-center w-60 pr-20">
+          <span className="text-sm">SPECS</span>
+          <span>durable and rugged enough to go anywhere.</span>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,51 @@
+import { ChevronDown, Dot, Home } from "lucide-react";
+
 const navItems = ["Vehicles", "Energy", "Charging", "Discover"];
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full flex justify-between items-start px-8 font-sans cursor-pointer z-50">
-      <span className="font-orbitron pt-2 text-xl">cybertruck</span>
-      <ul className="navbar-shape inline-flex bg-white text-black space-x-5 py-4 px-20 text-[8px] uppercase font-orbitron">
+    <div className="fixed top-0 w-full flex justify-center lg:justify-between items-start px-8 font-sans cursor-pointer z-50">
+      <span className="font-orbitron pt-2 text-xl w-60 hidden lg:block">
+        cybertruck
+      </span>
+      <ul className="navbar-shape hidden lg:inline-flex bg-white text-black space-x-2 py-3 px-20 text-[8px] uppercase font-orbitron slide-down-delay">
         {navItems.map((item, idx) => (
-          <li key={idx}>{item}</li>
+          <li
+            key={idx}
+            className="font-semibold flex items-center justify-center text-gray-500/60 hover:text-gray-800"
+          >
+            <Dot className="size-4" />
+            {item}
+          </li>
         ))}
       </ul>
-      <span className="space-x-4 py-2 text-xs">
-        <button className="uppercase bg-transparent text-amber-500 border-amber-500 border px-5 py-1 rounded-full">
-          LOGIN
+
+      <div className="navbar-shape bg-white text-black py-2 md:py-3 px-16 md:px-28 text-lg font-orbitron flex items-center justify-center slide-down-delay lg:hidden">
+        cybertruck
+      </div>
+
+      <span className="space-x-4 py-2 text-xs w-60 hidden lg:flex justify-end">
+        <button className="uppercase bg-transparent text-white border-white border pl-2 pr-1 py-1 rounded-full">
+          <span className="w-full flex items-center space-x-2">
+            <span className="flex items-center">
+              <ChevronDown className="size-2 mr-1" /> <span>LOGIN</span>
+            </span>
+            <div className="h-5 w-5 rounded-full overflow-hidden">
+              <img
+                src="/user.avif"
+                className="h-full w-full object-cover"
+                alt=""
+              />
+            </div>
+          </span>
         </button>
-        <button className="uppercase bg-transparent text-amber-500 border-amber-500 border px-5 py-1 rounded-full">
-          MENU
+        <button className="uppercase bg-white text-black border-black border pl-2 pr-1 py-1 rounded-full">
+          <span className="w-full flex items-center space-x-2">
+            <span className="pl-2">MENU</span>
+            <div className="h-5 w-5 rounded-full overflow-hidden border-black border-[0.2px] text-white bg-black flex justify-center items-center">
+              <Home className="size-3 object-cover" />
+            </div>
+          </span>
         </button>
       </span>
     </div>
